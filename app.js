ebf26a424f82afcb2ad9708017fee60880f659ca
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2021-11-08 12:56:26 -0300

    added basic server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const port = process.env.DEV_PORT || 4000;
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, console.log(`Server is listening on port ${port}`));
