eacba7c73a912a0a9e8062bac6b0ff0ad8f51dcd
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2021-11-09 16:46:02 -0300

    added mongo db atlas connection configuration

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import tasksRoutes from './routes/tasksRoutes.js';
+import { connectDB } from './db/connect.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
 // API endpoint configuration
+// TO-DO: keep this variables as enviromental variables (?)
 const APICurrentVersion = 'v1';
 const APIEndpointStart = `/api/${APICurrentVersion}`;
+
+// DB Connection configuration
+connectDB(process.env.DB_CONNECTION_STRING)
+  .then(() => {
+    const PORT = process.env.DEV_PORT || 4000;
+    console.log('DB Connected');
+    app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
+  })
+  .catch(error => console.log(error));
 
 // Middleware
 app.use(express.json());
@@ -15,5 +26,4 @@ app.use(express.json());
 // Routing
 app.use(`${APIEndpointStart}/tasks`, tasksRoutes);
 
-const PORT = process.env.DEV_PORT || 4000;
-app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
+
