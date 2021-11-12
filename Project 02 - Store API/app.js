b60e28f15eddd2c94768ea86a8d24bd832622311
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2021-11-12 16:22:08 -0300

    added connection to DB

--- Project 02 - Store API/app.js
+++ Project 02 - Store API/app.js
@@ -1,1 +1,14 @@
-console.log('04 Store API')
+import express from 'express';
+import { connectDB } from './db/connect.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+connectDB(process.env.DB_CONNECTION_STRING)
+  .then(() => {
+    const PORT = process.env.PORT || 4000;
+    console.log('DB connected')
+    app.listen(PORT, () => console.log(`Server is listening to port ${PORT}`))
+  })
+  .catch(error => console.log(error))
