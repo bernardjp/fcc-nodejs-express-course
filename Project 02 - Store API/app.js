308277b25b7eb992ae4401934419c9b18b49adf4
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2021-11-12 18:28:49 -0300

    added base server setup: base route, and error handling

--- Project 02 - Store API/app.js
+++ Project 02 - Store API/app.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import { connectDB } from './db/connect.js';
+import { errorHandlerMiddleware } from './middleware/error-handler.js';
+import { notFound } from './middleware/not-found.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+
+// Body-parser
+app.use(express.json());
+
+// Routing
+app.get('/', (req, res) => res.send('<h1>STORE API</h1><a href="/api/v1/products">Products route</a>'));
+
+// Error Handling
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 connectDB(process.env.DB_CONNECTION_STRING)
   .then(() => {
