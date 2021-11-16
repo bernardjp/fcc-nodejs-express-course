5921fa0cc4721a66afb4e13bbb154fd6314b66d0
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2021-11-16 15:10:18 -0300

    converted from CommonJS to Modules

--- Project 03 - JWT Basics/app.js
+++ Project 03 - JWT Basics/app.js
@@ -1,11 +1,19 @@
-require('dotenv').config();
-require('express-async-errors');
+import express from 'express';
+import 'express-async-errors';
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+import connectDB from './db/connect.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const express = require('express');
+// require('dotenv').config();
+// require('express-async-errors');
+
+// const express = require('express');
 const app = express();
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+// const notFoundMiddleware = require('./middleware/not-found');
+// const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.static('./public'));
