5750942bd31ae556a0a5f392dcca68e71ce6c189
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2021-11-19 20:31:24 -0300

    added user controller and user model with password hashing middleware

--- Project 04 - Job API/controllers/auth.js
+++ Project 04 - Job API/controllers/auth.js
@@ -1,6 +1,11 @@
+import UserModel from '../models/User.js';
+import { StatusCodes } from 'http-status-codes';
+// import jwt from 'jsonwebtoken';
+// import { BadRequestError } from '../errors';
 
 const register = async (req, res) => {
-  res.send('Registered User');
+  const newUser = await UserModel.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ msg: 'Success: User created.', data: newUser });
 }
 
 const login = async (req, res) => {
---
+++ Project 04 - Job API/models/User.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    minlength: 3,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email'
+    ],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6,
+  }
+});
+
+userSchema.pre('save', async function() {
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+export default UserModel;
