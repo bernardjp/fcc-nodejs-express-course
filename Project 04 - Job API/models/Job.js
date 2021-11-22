7667501d3958008486a4b68296707de5e551bcd8
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2021-11-22 18:37:10 -0300

    Added Job Model

---
+++ Project 04 - Job API/models/Job.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const jobSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, 'Please provide a company name'],
+    maxlength: 50
+  },
+  position: {
+    type: String,
+    required: [true, 'Please provide a position description'],
+    maxlength: 255
+  },
+  status: {
+    type: String,
+    enum: ['interview', 'declined', 'pending'],
+    default: 'pending'
+  },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Please provide a user']
+  }
+}, { timestamps: true });
+
+const JobModel = mongoose.model('Job', jobSchema);
+
+export default JobModel;
