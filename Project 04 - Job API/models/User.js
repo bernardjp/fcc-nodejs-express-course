8aa9b2345914dfac492571fd7c8cc1d169af12da
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2021-11-22 16:30:37 -0300

    Added login controller with password validation from the User Model

--- Project 04 - Job API/models/User.js
+++ Project 04 - Job API/models/User.js
@@ -35,6 +35,11 @@ userSchema.methods.createJWT = function() {
   return jwt.sign({ userID: this._id, name: this.name }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME });
 }
 
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+}
+
 const UserModel = mongoose.model('User', userSchema);
 
 export default UserModel;
