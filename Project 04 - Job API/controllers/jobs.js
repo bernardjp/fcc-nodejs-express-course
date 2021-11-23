9ce408957a239a8ae33ea73f2cdf4b5624bf1a5b
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2021-11-23 19:01:23 -0300

    Added implementation to all CRUD operations in the Job Controller

--- Project 04 - Job API/controllers/jobs.js
+++ Project 04 - Job API/controllers/jobs.js
@@ -1,22 +1,56 @@
+import JobModel from '../models/Job.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
 
-const getAllJobs = (req, res) => {
-  res.send('Getting all jobs');
+const getAllJobs = async (req, res) => {
+  const jobList = await JobModel.find({ createdBy: req.user.userID }).sort('createdAt');
+  res.status(StatusCodes.OK).json({ jobs: jobList, count: jobList.length });
 }
 
-const getJob= (req, res) => {
-  res.send('Getting a single job');
+const getJob= async (req, res) => {
+  const { 
+    user: { userID },
+    params: { id: jobID }
+  } = req;
+
+  const job = await JobModel.findOne({ _id: jobID, createdBy: userID });
+  if(!job) throw new NotFoundError(`Job with ID: ${jobID} not found`);
+
+  res.status(StatusCodes.OK).json({ job });
 }
 
-const createJob= (req, res) => {
-  res.send('Creating a single job');
+const createJob= async (req, res) => {
+  req.body.createdBy = req.user.userID;
+  
+  const newJob = await JobModel.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job: newJob });
 }
 
-const updateJob= (req, res) => {
-  res.send('Updating a single job');
+const updateJob= async (req, res) => {
+  const { 
+    user: { userID },
+    params: { id: jobID },
+    body: { company, position }
+  } = req;
+
+  if(!company || !position) throw new BadRequestError('Company and position must be provided');
+
+  const updatedJob = await JobModel.findOneAndUpdate({ _id: jobID, createdBy: userID }, req.body, { new: true, runValidators: true });
+  if(!updatedJob) throw new NotFoundError(`Job with ID: ${jobID} not found`);
+
+  res.status(StatusCodes.OK).json({ job: updatedJob });
 }
 
-const deleteJob= (req, res) => {
-  res.send('Deleting a single job');
+const deleteJob= async (req, res) => {
+  const { 
+    user: { userID },
+    params: { id: jobID }
+  } = req;
+
+  const deletedJob = await JobModel.findOneAndDelete({ _id: jobID, createdBy: userID });
+  if(!deletedJob) throw new NotFoundError(`Job with ID: ${jobID} not found`);
+
+  res.status(StatusCodes.OK).json({ msg: 'Job deleted' });
 }
 
 export {
