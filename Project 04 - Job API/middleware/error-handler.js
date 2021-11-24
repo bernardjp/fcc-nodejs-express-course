05f660e092d2f3fb8e63032769d039c3f1ec936f
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2021-11-24 15:30:43 -0300

    Added custom error handlers for MongoDB - Mongoose errors

--- Project 04 - Job API/middleware/error-handler.js
+++ Project 04 - Job API/middleware/error-handler.js
@@ -1,11 +1,32 @@
-import { CustomAPIError } from '../errors/index.js';
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  console.log(err);
+
+  const customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong, try again later'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  // Duplication Error.
+  if(err.code && err.code === 11000) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = `Invalid value entered for ${Object.keys(err.keyValue)}. Please enter another value.`;
+  }
+
+  // Validation Error.
+  if(err.name === 'ValidationError') {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = Object.values(err.errors).map(item => item.message).join('. ');
+  }
+
+  // Cast Error.
+  if(err.name === 'CastError') {
+    customError.statusCode = StatusCodes.NOT_FOUND;
+    customError.msg = `Invalid value. No resource found with ${err.path}: ${err.value}.`
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 }
 
 export default errorHandlerMiddleware;
